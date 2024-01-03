b0e4edebd8c0a62fc3b1bc0d350edb17984fa312
SeanLimHH <sean123lim@gmail.com>
2024-01-03 01:59:15 +0000

    completed

---
+++ Mod26.py
@@ -0,0 +1,21 @@
+'''
+Description:
+Cryptography can be easy, do you know what ROT13 is?
+
+Solution:
+Caesar's cipher just involves translating (shifting) characters down a fixed keyset. Alphabetical traditionally.
+ROT13 uses the following lookup table, so there are 26 characters only
+ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
+Also, doing ROT13 twice will yield the original; ROT13 is a reciprocal cipher
+'''
+
+alphabetKeyset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+wordToShift = input("To shift by 13 characters:")
+newWord = ''
+for character in wordToShift:
+    found = alphabetKeyset.find(character)
+    if found != -1:
+        newWord += alphabetKeyset[(found+13)%26]
+    else:
+        newWord += character
+print(newWord)
